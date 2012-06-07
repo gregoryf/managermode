27f3828c3c74227912e8d2c56debf96ea7043421
Greg Fisher <gregorynfisher@gmail.com>
2012-06-07 08:51:39 -0400

    fixed sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash.now.alert = "Invalid username or password."
+      render "new"
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      redirect_to root_url, notice: 'Logged out.'
+    else
+      render "new"
+    end
+  end
+end
