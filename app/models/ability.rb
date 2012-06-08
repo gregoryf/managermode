d1e0f06af1a875af930726be44dfaf57dac3606a
Greg Fisher <gregorynfisher@gmail.com>
2012-06-07 22:26:18 -0400

    Cancan sorted out

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    
+    user ||= User.new
+    can :read, :all
+    can :create, User
+    can :update, User, id: user.id
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
